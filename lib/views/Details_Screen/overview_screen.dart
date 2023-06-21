e66c0f40daa9e8c663afc762e1178deabf3b0289
hamidhosen42 <mdhamidhosen8444@gmail.com>
2023-06-21 15:29:47 +0600

    add-location

--- lib/views/Details_Screen/overview_screen.dart
+++ lib/views/Details_Screen/overview_screen.dart
@@ -1,10 +1,12 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors_in_immutables, prefer_const_literals_to_create_immutables
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors_in_immutables, prefer_const_literals_to_create_immutables, prefer_const_constructors, unused_local_variable, prefer_interpolation_to_compose_strings, non_constant_identifier_names, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tour_application/views/Details_Screen/price_duration_tile.dart';
 
+import '../Googele Map/place_location.dart';
 import 'services_tile.dart';
 
 class OverviewScreen extends StatefulWidget {
@@ -12,9 +14,7 @@ class OverviewScreen extends StatefulWidget {
   final String location;
   final String duration;
   OverviewScreen(
-      {required this.location,
-      required this.duration,
-      required this.place});
+      {required this.location, required this.duration, required this.place});
 
   @override
   State<OverviewScreen> createState() => _OverviewScreenState();
@@ -40,7 +40,48 @@ class _OverviewScreenState extends State<OverviewScreen> {
             ),
             Row(
               children: [
-                const Icon(Icons.location_on_outlined),
+                IconButton(
+                  onPressed: () async {
+                    var address = widget.place + " " + widget.location;
+
+                    String latitude_longitude_addres = "";
+                    String query_address = "";
+
+                    List<Location> locations =
+                        await locationFromAddress(address);
+
+                    List<Placemark> placemarks = await placemarkFromCoordinates(
+                        locations.last.latitude, locations.last.longitude);
+
+                    setState(() {
+                      // latitude_longitude_addres =
+                      //     locations.last.latitude.toString() +
+                      //         "   " +
+                      //         locations.last.longitude.toString();
+                      // query_address =
+                      //     placemarks.reversed.last.country.toString();
+
+                      var latitude_address =
+                          double.parse(locations.last.latitude.toString());
+
+                      var longitude_address =
+                          double.parse(locations.last.longitude.toString());
+
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => PlaceLocation(
+                                    latitude_address: latitude_address,
+                                    longitude_address: longitude_address,
+                                    location:widget.location
+                                  )));
+                    });
+                  },
+                  icon: Icon(
+                    Icons.location_on_outlined,
+                    color: Colors.blue,
+                  ),
+                ),
                 Expanded(
                   child: Text(
                     widget.location,
@@ -74,8 +115,8 @@ class _OverviewScreenState extends State<OverviewScreen> {
               ),
             ),
             SizedBox(
-                  height: 20.h,
-                ),
+              height: 20.h,
+            ),
             Column(
               children: [
                 Row(
@@ -93,10 +134,8 @@ class _OverviewScreenState extends State<OverviewScreen> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    servicesTile(Icons.pool, 'Swimming pool',
-                        context, () {}),
-                    servicesTile(
-                        Icons.fitness_center, 'Gym', context, () {}),
+                    servicesTile(Icons.pool, 'Swimming pool', context, () {}),
+                    servicesTile(Icons.fitness_center, 'Gym', context, () {}),
                   ],
                 ),
                 SizedBox(
@@ -105,10 +144,9 @@ class _OverviewScreenState extends State<OverviewScreen> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    servicesTile(Icons.food_bank_outlined, 'Food',
-                        context, () {}),
                     servicesTile(
-                        Icons.fitness_center, 'Food', context, () {}),
+                        Icons.food_bank_outlined, 'Food', context, () {}),
+                    servicesTile(Icons.fitness_center, 'Food', context, () {}),
                   ],
                 ),
               ],
